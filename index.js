9177a1914b610e5501533c16dae6f208e54d86a2
sakakendo <sakakendo0321@gmail.com>
2020-03-02 17:41:21 +0900

    get port from environ

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000
+const port = parseInt(process.env.PORT) || 3000
 
 /*
 const mongo_uri = 'mongodb://localhost/express-rest' 
