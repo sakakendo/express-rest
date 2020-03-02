3eb817f199dc8f57d0ab3634a19eed967202e3d3
sakakendo <sakakendo0321@gmail.com>
2020-03-02 18:00:04 +0900

    load mongodb_url from environment

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const mongoose = require('mongoose');
 const app = express();
 const port = parseInt(process.env.PORT) || 3000
 
-/*
-const mongo_uri = 'mongodb://localhost/express-rest' 
-mongoose.connect(mongo_uri, function(err){
+const mongo_uri = process.env.MONGODB_URI
+if(!mongo_uri) throw 'mongodb uri is undefined';
+mongoose.connect(mongo_uri, {useNewUrlParser: true}, function(err){
   if(err){
     throw err
   }else{
     console.log(`successfully connected to mongodb: ${mongo_uri}`)
   }
 })
-*/
 
 var router = express.Router();
 var api = function(router){
