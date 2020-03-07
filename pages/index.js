c1f05a603cb3128a2edfeffb7f615d083ad0b80c
sakakendo <sakakendo0321@gmail.com>
2020-03-08 01:47:40 +0900

    beautify

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import Layout from '../components/layout';
 import { RootContext } from '../components/Context';
 
+const ENTER_KEY = 13;
+const ESCAPE_KEY = 27;
+
 function getTodo(token, cb) {
   axios.get('/api/task', {
     headers: {
@@ -42,58 +45,89 @@ function updateTask(token, task, cb, fb){
   });
 }
 
-function TodoItem({ item, mode, updateItem }) {
+function TodoItem({ item, mode, editing, onEdit, finishEdit, updateItem }) {
   const { title, note, completed } = item;
   const {user} = useContext(RootContext);
   const {token} = user;
   if ((mode === 1 && completed) || (mode === 2 && !completed)) return null;
   return (
-    <div className="item">
-      <label className="container">
-        <input 
-          type="checkbox"
-          checked={completed? 'checked': ''} 
-          onChange={()=>{
-          updateTask(token, {...item, completed: !completed}, 
-            (res) => updateItem(res), console.error);
-        }}></input>
-        <span className="checkmark"/>
+      <li className={`${editing} ${completed? 'completed': ''}`}>
+        <div className="view">
+          <input 
+            type="checkbox"
+            className="toggle"
+            checked={completed? 'checked': ''} 
+            onChange={()=>{
+            updateTask(token, {...item, completed: !completed}, 
+              (res) => updateItem(res), console.error);
+          }}></input>
+          <label onDoubleClick={onEdit}>{title}</label>
+        </div>
       <input
         type="text"
+        className="edit"
         value={title}
         onBlur={(e)=>{
-          updateTask(token, item, console.log, console.error)
+          updateTask(token, item, console.log, console.error);
+          finishEdit();
         }}
         onChange={(e) => {
           updateItem({ ...item, title: e.target.value })
-        }}></input>
-      </label>
-    </div>
+        }}
+        onKeyDown={(e)=>{
+          if(e.which === ESCAPE_KEY) finishEdit();
+          else if(e.which === ENTER_KEY){
+            updateTask(token, item, console.log, console.error);
+            finishEdit();
+          }
+        }}
+        ></input>
+      </li>
   )
 }
 
 function TodoList({ items, setItems }) {
   const [mode, setMode] = useState(0);
+  const [edit, setEdit] = useState(null);
   return (
-    <>
-      {
-        items.map((item, i) =>
-          <TodoItem key={i} item={item} mode={mode} updateItem={(item) => {
-            const _items = items.slice()
-            _items[i] = item;
-            setItems(_items)
-          }} />)
-      }
-      <button  
-        className={`${mode === 0? 'active': ''}`} 
-        onClick={() => setMode(0)}>all</button>
-      <button 
-        className={`${mode === 1? 'active': ''}`} 
-        onClick={() => setMode(1)}>active</button>
-      <button 
-        className={`${mode === 2? 'active': ''}`} 
-        onClick={() => setMode(2)}>completed</button>
-    </>
+    <section className="main">
+      <ul className="todo-list">
+        {
+          items.map((item, i) =>
+            <TodoItem key={i} item={item} mode={mode} 
+              editing={ i === edit ? 'editing': ''} 
+              onEdit={()=>setEdit(i)} 
+              finishEdit={()=>setEdit(null)} 
+              updateItem={(item) => {
+                const _items = items.slice()
+                _items[i] = item;
+                setItems(_items)
+              }} />)
+        }
+      </ul>
+      <footer className="footer">
+        <span></span>
+        <ul className="filters">
+          <li>
+            <a 
+              className={`${mode === 0? 'active': ''}`} 
+              onClick={()=>setMode(0)}>All</a>
+          </li>
+          {' '}
+          <li>
+            <a 
+              className={`${mode === 1? 'active': ''}`} 
+              onClick={()=>setMode(1)} >Active</a>
+          </li>
+          {' '}
+          <li>
+            <a 
+              className={`${mode === 2? 'active': ''}`} 
+              onClick={()=>setMode(2)}>Completed</a>
+          </li>
+        </ul>
+      </footer>
+    </section>
   )
 }
 
@@ -109,7 +143,9 @@ function NewTodoForm({ onCreate }) {
     }>
       <input
         type="text"
+        className="new-todo edit"
         value={title}
+        autoFocus={true}
         onChange={(e) => setTitle(e.target.value)}
         placeholder="input new task"></input>
     </form>
@@ -130,12 +166,9 @@ function Todo() {
         note: '',
         completed: false
       };
-      createTask(token, task, (resource)=>{
-        console.log(resource);
-        setItems([resource].concat(items))
-      }, (error)=>{
-        console.error(error);
-      });
+      createTask(token, task, 
+        (resource)=> setItems([resource].concat(items)) , 
+        (error)=> console.error(error));
     }} />
     <TodoList
       items={items}
@@ -146,7 +179,10 @@ function Todo() {
 function Dashboard() {
   return (
     <div className="dashboard">
-      <Todo />
+      <section className="todoapp">
+        <h1>todo app</h1>
+        <Todo />
+      </section>
     </div>
   )
 }
